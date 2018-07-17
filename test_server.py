16d74be79a462344b8bfede78f665a6011ec97e1
刘亚斌 <1512368024@qq.com>
2018-07-17 16:20:42 +0800

    tijiao

---
+++ test_server.py
@@ -0,0 +1,20 @@
+#server
+import socket
+from threading import Thread
+def forward(conn):
+    while True:
+            data=conn.recv(1024)
+            conn.send(data)
+            print(data.decode('utf8'))
+            if (data.decode('utf8'))=='bye':
+                conn.close()
+                break
+            
+if __name__=='__main__':
+    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.bind(('127.0.0.1',4000))
+    s.listen(5)
+    while True:
+        ss,addr=s.accept()
+        t=Thread(target=forward,args=(ss,))
+        t.start()
